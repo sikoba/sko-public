359e6b7b38e728ba8e16e5e78170471a81baa359
Katerina <katerinalupacheva@gmail.com>
2020-07-21 13:06:06 +0300

    siteConfig updated

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'sikobaPay User Documentation', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://sikoba.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://sikoba.github.io', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs-site',
-  organizationName: 'Sikoba',
+  projectName: 'sko-public',
+  organizationName: 'sikoba',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
