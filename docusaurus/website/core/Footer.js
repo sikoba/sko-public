705155c870022e66d13950256ccd87985338f9af
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-07-24 11:55:59 +0300

    Footer updated

--- docusaurus/website/core/Footer.js
+++ docusaurus/website/core/Footer.js
@@ -24,7 +24,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="sitemap">
+        {/* <section className="sitemap">
           <a href={this.props.config.baseUrl} className="nav-home">
             {this.props.config.footerIcon && (
               <img
@@ -116,7 +116,7 @@ class Footer extends React.Component {
             width="170"
             height="45"
           />
-        </a>
+        </a> */}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
